056703b0cc132c97114b9cdae336fe8567256e1f
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-27 15:28:59 +0300

    final touches

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -72,7 +72,7 @@ export default function GuessGameUI() {
           placeholder="Enter your guess number"
           onChange={handleGuessInput}
         />
-        <p className={currentGuessState.win ?"trials"  : "no-trials"}>
+        <p className={currentGuessState.win ? "trials" : ""}>
           {currentGuessState.gameStatus}
         </p>
         <button
